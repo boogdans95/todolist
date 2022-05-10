19cbec7f2ad26dcef86fc35e50f98eb19db97552
Bogdan <boogdans95@gmail.com>
2022-05-10 22:42:45 +0300

    Initial commit

---
+++ src/components/ToDoForm.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+function ToDoForm({ addTask }) {
+    const [userInput, setUserInput] = useState ('')
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+        
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addTask(userInput)
+        setUserInput("")
+
+    }
+
+    const hendleKeyPress = (e) => {
+        if(e.key === "Enter") {
+            handleSubmit(e)
+        }
+        
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                value={userInput}
+                type="text"
+                onChange={handleChange}
+                onKeyDown={hendleKeyPress}
+                placeholder="Введите значение..."
+            />
+            <button>Сохранить</button>
+
+        </form>
+    )
+}
+
+
+export default ToDoForm
